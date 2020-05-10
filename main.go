aa6f70eb5def3a9a7ac0ed0ee4d96501fb12ad46
Akezhan <21172@iitu.kz>
2020-05-10 22:24:27 +0600

    fix

--- main.go
+++ main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"log"
-	c "github.com/AkezhanOb1/diplomaProject/services/client/orders"
-
 )
 
 func main() {
 	log.Println("Hello world")
-	log.Println(c.GetBusinessServiceOrders())
 }
